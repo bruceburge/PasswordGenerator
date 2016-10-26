0b40cc77ecee2b71741013289ebcba3695d0ea99
Bruce Burge <bruceburge@users.noreply.github.com>
2016-10-26 01:13:48 -0500

    Temp Trash sql tables to start playing with data.
    These schemes will clearly have to be revised.

---
+++ data/sql/Claws7Tags.sql
@@ -0,0 +1,965 @@
+/* CREATE TABLE */
+CREATE TABLE Claws7Tags(
+Code VARCHAR(10),
+Description VARCHAR(100)
+);
+
+/* INSERT QUERY NO: 1 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'APPGE ', 'possessive pronoun'
+);
+
+/* INSERT QUERY NO: 2 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'AT ', 'article (e.g. the'
+);
+
+/* INSERT QUERY NO: 3 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'AT1 ', 'singular article (e.g. a'
+);
+
+/* INSERT QUERY NO: 4 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'BCL ', 'before-clause marker (e.g. in order (that)'
+);
+
+/* INSERT QUERY NO: 5 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'CC ', 'coordinating conjunction (e.g. and'
+);
+
+/* INSERT QUERY NO: 6 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'CCB ', 'adversative coordinating conjunction ( but)'
+);
+
+/* INSERT QUERY NO: 7 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'CS ', 'subordinating conjunction (e.g. if'
+);
+
+/* INSERT QUERY NO: 8 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'CSA ', 'as (as conjunction)'
+);
+
+/* INSERT QUERY NO: 9 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'CSN ', 'than (as conjunction)'
+);
+
+/* INSERT QUERY NO: 10 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'CST ', 'that (as conjunction)'
+);
+
+/* INSERT QUERY NO: 11 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'CSW ', 'whether (as conjunction)'
+);
+
+/* INSERT QUERY NO: 12 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DA ', 'after-determiner or post-determiner capable of pronominal function (e.g. such'
+);
+
+/* INSERT QUERY NO: 13 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DA1 ', 'singular after-determiner (e.g. little'
+);
+
+/* INSERT QUERY NO: 14 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DA2 ', 'plural after-determiner (e.g. few'
+);
+
+/* INSERT QUERY NO: 15 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DAR ', 'comparative after-determiner (e.g. more'
+);
+
+/* INSERT QUERY NO: 16 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DAT ', 'superlative after-determiner (e.g. most'
+);
+
+/* INSERT QUERY NO: 17 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DB ', 'before determiner or pre-determiner capable of pronominal function (all'
+);
+
+/* INSERT QUERY NO: 18 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DB2 ', 'plural before-determiner ( both)'
+);
+
+/* INSERT QUERY NO: 19 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DD ', 'determiner (capable of pronominal function) (e.g any'
+);
+
+/* INSERT QUERY NO: 20 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DD1 ', 'singular determiner (e.g. this'
+);
+
+/* INSERT QUERY NO: 21 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DD2 ', 'plural determiner ( these'
+);
+
+/* INSERT QUERY NO: 22 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DDQ ', 'wh-determiner (which'
+);
+
+/* INSERT QUERY NO: 23 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DDQGE ', 'wh-determiner'
+);
+
+/* INSERT QUERY NO: 24 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'DDQV ', 'wh-ever determiner'
+);
+
+/* INSERT QUERY NO: 25 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'EX ', 'existential there'
+);
+
+/* INSERT QUERY NO: 26 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'FO ', 'formula'
+);
+
+/* INSERT QUERY NO: 27 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'FU ', 'unclassified word'
+);
+
+/* INSERT QUERY NO: 28 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'FW ', 'foreign word'
+);
+
+/* INSERT QUERY NO: 29 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'GE ', 'germanic genitive marker - (\' or\'s)'
+);
+
+/* INSERT QUERY NO: 30 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'IF ', 'for (as preposition)'
+);
+
+/* INSERT QUERY NO: 31 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'II ', 'general preposition'
+);
+
+/* INSERT QUERY NO: 32 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'IO ', 'of (as preposition)'
+);
+
+/* INSERT QUERY NO: 33 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'IW ', 'with'
+);
+
+/* INSERT QUERY NO: 34 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'JJ ', 'general adjective'
+);
+
+/* INSERT QUERY NO: 35 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'JJR ', 'general comparative adjective (e.g. older'
+);
+
+/* INSERT QUERY NO: 36 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'JJT ', 'general superlative adjective (e.g. oldest'
+);
+
+/* INSERT QUERY NO: 37 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'JK ', 'catenative adjective (able in be able to'
+);
+
+/* INSERT QUERY NO: 38 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'MC ', 'cardinal number'
+);
+
+/* INSERT QUERY NO: 39 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'MC1 ', 'singular cardinal number (one)'
+);
+
+/* INSERT QUERY NO: 40 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'MC2 ', 'plural cardinal number (e.g. sixes'
+);
+
+/* INSERT QUERY NO: 41 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'MCGE ', 'genitive cardinal number'
+);
+
+/* INSERT QUERY NO: 42 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'MCMC ', 'hyphenated number (40-50'
+);
+
+/* INSERT QUERY NO: 43 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'MD ', 'ordinal number (e.g. first'
+);
+
+/* INSERT QUERY NO: 44 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'MF ', 'fraction'
+);
+
+/* INSERT QUERY NO: 45 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'ND1 ', 'singular noun of direction (e.g. north'
+);
+
+/* INSERT QUERY NO: 46 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NN ', 'common noun'
+);
+
+/* INSERT QUERY NO: 47 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NN1 ', 'singular common noun (e.g. book'
+);
+
+/* INSERT QUERY NO: 48 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NN2 ', 'plural common noun (e.g. books'
+);
+
+/* INSERT QUERY NO: 49 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NNA ', 'following noun of title (e.g. M.A.)'
+);
+
+/* INSERT QUERY NO: 50 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NNB ', 'preceding noun of title (e.g. Mr.'
+);
+
+/* INSERT QUERY NO: 51 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NNL1 ', 'singular locative noun (e.g. Island'
+);
+
+/* INSERT QUERY NO: 52 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NNL2 ', 'plural locative noun (e.g. Islands'
+);
+
+/* INSERT QUERY NO: 53 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NNO ', 'numeral noun'
+);
+
+/* INSERT QUERY NO: 54 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NNO2 ', 'numeral noun'
+);
+
+/* INSERT QUERY NO: 55 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NNT1 ', 'temporal noun'
+);
+
+/* INSERT QUERY NO: 56 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NNT2 ', 'temporal noun'
+);
+
+/* INSERT QUERY NO: 57 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NNU ', 'unit of measurement'
+);
+
+/* INSERT QUERY NO: 58 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NNU1 ', 'singular unit of measurement (e.g. inch'
+);
+
+/* INSERT QUERY NO: 59 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NNU2 ', 'plural unit of measurement (e.g. ins.'
+);
+
+/* INSERT QUERY NO: 60 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NP ', 'proper noun'
+);
+
+/* INSERT QUERY NO: 61 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NP1 ', 'singular proper noun (e.g. London'
+);
+
+/* INSERT QUERY NO: 62 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NP2 ', 'plural proper noun (e.g. Browns'
+);
+
+/* INSERT QUERY NO: 63 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NPD1 ', 'singular weekday noun (e.g. Sunday)'
+);
+
+/* INSERT QUERY NO: 64 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NPD2 ', 'plural weekday noun (e.g. Sundays)'
+);
+
+/* INSERT QUERY NO: 65 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NPM1 ', 'singular month noun (e.g. October)'
+);
+
+/* INSERT QUERY NO: 66 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'NPM2 ', 'plural month noun (e.g. Octobers)'
+);
+
+/* INSERT QUERY NO: 67 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PN ', 'indefinite pronoun'
+);
+
+/* INSERT QUERY NO: 68 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PN1 ', 'indefinite pronoun'
+);
+
+/* INSERT QUERY NO: 69 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PNQO ', 'objective wh-pronoun (whom)'
+);
+
+/* INSERT QUERY NO: 70 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PNQS ', 'subjective wh-pronoun (who)'
+);
+
+/* INSERT QUERY NO: 71 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PNQV ', 'wh-ever pronoun (whoever)'
+);
+
+/* INSERT QUERY NO: 72 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PNX1 ', 'reflexive indefinite pronoun (oneself)'
+);
+
+/* INSERT QUERY NO: 73 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPGE ', 'nominal possessive personal pronoun (e.g. mine'
+);
+
+/* INSERT QUERY NO: 74 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPH1 ', '3rd person sing. neuter personal pronoun (it)'
+);
+
+/* INSERT QUERY NO: 75 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPHO1 ', '3rd person sing. objective personal pronoun (him'
+);
+
+/* INSERT QUERY NO: 76 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPHO2 ', '3rd person plural objective personal pronoun (them)'
+);
+
+/* INSERT QUERY NO: 77 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPHS1 ', '3rd person sing. subjective personal pronoun (he'
+);
+
+/* INSERT QUERY NO: 78 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPHS2 ', '3rd person plural subjective personal pronoun (they)'
+);
+
+/* INSERT QUERY NO: 79 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPIO1 ', '1st person sing. objective personal pronoun (me)'
+);
+
+/* INSERT QUERY NO: 80 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPIO2 ', '1st person plural objective personal pronoun (us)'
+);
+
+/* INSERT QUERY NO: 81 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPIS1 ', '1st person sing. subjective personal pronoun (I)'
+);
+
+/* INSERT QUERY NO: 82 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPIS2 ', '1st person plural subjective personal pronoun (we)'
+);
+
+/* INSERT QUERY NO: 83 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPX1 ', 'singular reflexive personal pronoun (e.g. yourself'
+);
+
+/* INSERT QUERY NO: 84 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPX2 ', 'plural reflexive personal pronoun (e.g. yourselves'
+);
+
+/* INSERT QUERY NO: 85 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'PPY ', '2nd person personal pronoun (you)'
+);
+
+/* INSERT QUERY NO: 86 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RA ', 'adverb'
+);
+
+/* INSERT QUERY NO: 87 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'REX ', 'adverb introducing appositional constructions (namely'
+);
+
+/* INSERT QUERY NO: 88 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RG ', 'degree adverb (very'
+);
+
+/* INSERT QUERY NO: 89 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RGQ ', 'wh- degree adverb (how)'
+);
+
+/* INSERT QUERY NO: 90 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RGQV ', 'wh-ever degree adverb (however)'
+);
+
+/* INSERT QUERY NO: 91 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RGR ', 'comparative degree adverb (more'
+);
+
+/* INSERT QUERY NO: 92 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RGT ', 'superlative degree adverb (most'
+);
+
+/* INSERT QUERY NO: 93 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RL ', 'locative adverb (e.g. alongside'
+);
+
+/* INSERT QUERY NO: 94 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RP ', 'prep. adverb'
+);
+
+/* INSERT QUERY NO: 95 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RPK ', 'prep. adv.'
+);
+
+/* INSERT QUERY NO: 96 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RR ', 'general adverb'
+);
+
+/* INSERT QUERY NO: 97 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RRQ ', 'wh- general adverb (where'
+);
+
+/* INSERT QUERY NO: 98 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RRQV ', 'wh-ever general adverb (wherever'
+);
+
+/* INSERT QUERY NO: 99 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RRR ', 'comparative general adverb (e.g. better'
+);
+
+/* INSERT QUERY NO: 100 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RRT ', 'superlative general adverb (e.g. best'
+);
+
+/* INSERT QUERY NO: 101 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'RT ', 'quasi-nominal adverb of time (e.g. now'
+);
+
+/* INSERT QUERY NO: 102 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'TO ', 'infinitive marker (to)'
+);
+
+/* INSERT QUERY NO: 103 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'UH ', 'interjection (e.g. oh'
+);
+
+/* INSERT QUERY NO: 104 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VB0 ', 'be'
+);
+
+/* INSERT QUERY NO: 105 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VBDR ', 'were'
+);
+
+/* INSERT QUERY NO: 106 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VBDZ ', 'was'
+);
+
+/* INSERT QUERY NO: 107 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VBG ', 'being'
+);
+
+/* INSERT QUERY NO: 108 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VBI ', 'be'
+);
+
+/* INSERT QUERY NO: 109 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VBM ', 'am'
+);
+
+/* INSERT QUERY NO: 110 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VBN ', 'been'
+);
+
+/* INSERT QUERY NO: 111 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VBR ', 'are'
+);
+
+/* INSERT QUERY NO: 112 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VBZ ', 'is'
+);
+
+/* INSERT QUERY NO: 113 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VD0 ', 'do'
+);
+
+/* INSERT QUERY NO: 114 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VDD ', 'did'
+);
+
+/* INSERT QUERY NO: 115 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VDG ', 'doing'
+);
+
+/* INSERT QUERY NO: 116 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VDI ', 'do'
+);
+
+/* INSERT QUERY NO: 117 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VDN ', 'done'
+);
+
+/* INSERT QUERY NO: 118 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VDZ ', 'does'
+);
+
+/* INSERT QUERY NO: 119 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VH0 ', 'have'
+);
+
+/* INSERT QUERY NO: 120 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VHD ', 'had (past tense)'
+);
+
+/* INSERT QUERY NO: 121 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VHG ', 'having'
+);
+
+/* INSERT QUERY NO: 122 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VHI ', 'have'
+);
+
+/* INSERT QUERY NO: 123 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VHN ', 'had (past participle)'
+);
+
+/* INSERT QUERY NO: 124 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VHZ ', 'has'
+);
+
+/* INSERT QUERY NO: 125 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VM ', 'modal auxiliary (can'
+);
+
+/* INSERT QUERY NO: 126 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VMK ', 'modal catenative (ought'
+);
+
+/* INSERT QUERY NO: 127 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VV0 ', 'base form of lexical verb (e.g. give'
+);
+
+/* INSERT QUERY NO: 128 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VVD ', 'past tense of lexical verb (e.g. gave'
+);
+
+/* INSERT QUERY NO: 129 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VVG ', '-ing participle of lexical verb (e.g. giving'
+);
+
+/* INSERT QUERY NO: 130 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VVGK ', '-ing participle catenative (going in be going to)'
+);
+
+/* INSERT QUERY NO: 131 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VVI ', 'infinitive (e.g. to give... It will work...)'
+);
+
+/* INSERT QUERY NO: 132 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VVN ', 'past participle of lexical verb (e.g. given'
+);
+
+/* INSERT QUERY NO: 133 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VVNK ', 'past participle catenative (e.g. bound in be bound to)'
+);
+
+/* INSERT QUERY NO: 134 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'VVZ ', '-s form of lexical verb (e.g. gives'
+);
+
+/* INSERT QUERY NO: 135 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'XX ', 'not'
+);
+
+/* INSERT QUERY NO: 136 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'ZZ1 ', 'singular letter of the alphabet (e.g. A'
+);
+
+/* INSERT QUERY NO: 137 */
+INSERT INTO Claws7Tags(Code, Description)
+VALUES
+(
+'ZZ2 ', 'plural letter of the alphabet (e.g. A\'s'
+);
+
