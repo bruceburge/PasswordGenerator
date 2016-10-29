07a0a808e5b41bfaea4b5c5c03034de4bfdcd699
Bruce Burge <bruce@bruceburge.com>
2016-10-29 04:33:00 -0500

    Added new pattern

---
+++ PasswordGenerator/PasswordGenerator/PasswordPatterns/VerbTheNoun.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    class VerbTheNoun : IPasswordPattern
+    {
+
+        private Random rnd;
+
+        public void Initialize(Random random)
+        {
+            rnd = random;
+        }
+
+        public string GetPattern()
+        {
+            return string.Format(
+                "{0}.{1}.{2}",
+                WordLibs.UppercaseWords(WordLibs.Verbs[rnd.Next(WordLibs.Verbs.Length)]),
+                ".The.",
+                WordLibs.UppercaseWords(WordLibs.Nouns[rnd.Next(WordLibs.Nouns.Length)]));
+        }
+
+        public string GetPatternTitle()
+        {
+            return "Verb.The.Noun";
+        }
+
+
+    }
+}
